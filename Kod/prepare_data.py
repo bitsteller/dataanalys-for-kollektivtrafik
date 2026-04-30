82146cc858a09fe79241e51dea91500fbec22b04
Nils Breyer <nils.breyer@liu.se>
2026-04-30 13:26:45 +0200

    data

--- Kod/prepare_data.py
+++ Kod/prepare_data.py
@@ -23,5 +23,27 @@ trains = (
     .filter((pl.col("PlannedDwellTime") >= 0) & (pl.col("ActualDwellTime") >= 0)) #tar bort orimliga värden
     .filter((pl.col("PlannedDwellTime") <= 15) & (pl.col("ActualDwellTime") <= 60)) #tar bort extrema värden
 )
+
+# read reasons as a dictinary {code: description}
+reasons = pl.read_parquet("Data/Laboration 3/reasons.parquet")
+reasons_dict = {
+    row["Code"]: row["Level3Description"]
+    for row in reasons.iter_rows(named=True)
+}
+# add reasons_dict to trains
+trains = trains.with_columns(
+    pl.col("DeviationCodes")
+    .map_elements(
+        lambda codes: (
+            []
+            if codes is None
+            else [reasons_dict[code] for code in codes if code in reasons_dict]
+        ),
+        return_dtype=pl.List(pl.String),
+    )
+    .alias("Deviations")
+)
+
+
 # Write back to parquet
 trains.write_parquet("Data/Laboration 3/trains.parquet")
